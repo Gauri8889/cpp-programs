0b33d05b75e4a64ab34ccdbd6e35478ffcf29176
gauri <dahakegauri@4gmail.com>
2024-09-05 22:54:35 +0530

    top to bottum.cpp

---
+++ top to bottum.cpp
@@ -0,0 +1,15 @@
+// this is proof that bottum to top
+ #include<iostream>
+ using namespace std;
+
+ int main()
+ {
+     cout<<"Ashfaq sir\n";
+ }
+class cybrom
+ {
+     public:cybrom()
+     {
+         cout<<"cybrom class\n";
+     }
+ }c; //this is obj
